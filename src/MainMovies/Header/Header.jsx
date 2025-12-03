204a7fdf4f43739923a4bd27d45479f88030e0f7
walid <walidsaoud290@gmail.com>
2025-12-03 17:57:46 +0100

    new changes

--- src/MainMovies/Header/Header.jsx
+++ src/MainMovies/Header/Header.jsx
@@ -1,87 +1,78 @@
-import React, { useState } from 'react';
+import React, { createContext, useState } from 'react';
 import './Header.css';
 import ModalUser from '../Pages/ModalUser/ModalUser';
 
+export const context = createContext();
+
 const Header = () => {
-  const [image,setImage] = useState('vecteezy_default-profile-account-unknown-icon-black-silhouette_20765399.jpg');
-  const handleChangeImage = ()=>{
-    const handleImageChange = (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      setImage(URL.createObjectURL(file)); // preview image
-    }
-  };
-  }
+
+  const [image, setImage] = useState('vecteezy_default-profile-account-unknown-icon-black-silhouette_20765399.jpg');
+
   return (
-    <>
-      <nav className="navbar nvBarOrigin navbar-expand-lg rounded navbar-light">
-        <div className="container-fluid">
-          <a className="navbar-brand text-white" href="#">MovieShow</a>
+    <context.Provider value={{ image, setImage }}>
+      <>
+        <nav className="navbar nvBarOrigin navbar-expand-lg rounded navbar-light">
+          <div className="container-fluid">
+            <a className="navbar-brand text-white" href="#">MovieShow</a>
 
-          <button 
-            className="navbar-toggler bg-white" 
-            type="button" 
-            data-bs-toggle="collapse" 
-            data-bs-target="#navbarNav"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+            <button 
+              className="navbar-toggler bg-white"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
 
-          <div className="collapse navbar-collapse my-6" id="navbarNav">
+            <div className="collapse navbar-collapse my-6" id="navbarNav">
+              <ul className="navbar-nav nv">
+                <li className="nav-item"><a className="nav-link text-white" href="#">Home</a></li>
+                <li className="nav-item"><a className="nav-link text-white" href="#">Features</a></li>
+                <li className="nav-item"><a className="nav-link text-white" href="#">Pricing</a></li>
+              </ul>
 
-            <ul className="navbar-nav nv">
-              <li className="nav-item"><a className="nav-link text-white" href="#">Home</a></li>
-              <li className="nav-item"><a className="nav-link text-white" href="#">Features</a></li>
-              <li className="nav-item"><a className="nav-link text-white" href="#">Pricing</a></li>
-            </ul>
+              <div className="navbar-nav ms-auto">
+                <div className="nav-item dropdown">
+                  <a 
+                    className="nav-link d-flex align-items-center"
+                    href="#"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                  >
+                    <img 
+                      src={image}
+                      alt="Avatar"
+                      width="40"
+                      height="40"
+                      className="rounded-circle me-2"
+                    />
+                  </a>
 
-            <div className="navbar-nav ms-auto">
-              <div className="nav-item dropdown">
-                <a 
-                  className="nav-link d-flex align-items-center" 
-                  href="#" 
-                  role="button" 
-                  data-bs-toggle="dropdown" 
-                >
-                  <img 
-                    src={image}
-                    alt="Avatar"
-                    width="40"
-                    height="40"
-                    className="rounded-circle me-2"
-                  />
-                </a>
-
-                <ul className="dropdown-menu dropdown-menu-end">
-                  <li>
-                    <button 
-                      className="dropdown-item" 
-                      data-bs-toggle="modal" 
-                      data-bs-target="#modalUser"
-                    >
-                      My profile
-                    </button>
-                  </li>
-
-                  <li><button className="dropdown-item">Settings</button></li>
-
-                  <li><hr className="dropdown-divider" /></li>
-
-                  <li>
-                    <button className="dropdown-item text-danger">Logout</button>
-                  </li>
-                </ul>
+                  <ul className="dropdown-menu dropdown-menu-end">
+                    <li>
+                      <button 
+                        className="dropdown-item"
+                        data-bs-toggle="modal"
+                        data-bs-target="#modalUser"
+                      >
+                        My profile
+                      </button>
+                    </li>
+                    <li><button className="dropdown-item">Settings</button></li>
+                    <li><hr className="dropdown-divider" /></li>
+                    <li><button className="dropdown-item text-danger">Logout</button></li>
+                  </ul>
+                </div>
               </div>
+
             </div>
-
           </div>
-        </div>
-      </nav>
+        </nav>
 
-      {/* Render modal OUTSIDE the dropdown */}
-      <ModalUser id="modalUser" image={image} handleChangeImage={handleChangeImage}/>
-    </>
+        <ModalUser id="modalUser" />
+      </>
+    </context.Provider>
   );
-}
+};
 
 export default Header;
--- src/MainMovies/Pages/ModalUser/ModalUser.jsx
+++ src/MainMovies/Pages/ModalUser/ModalUser.jsx
@@ -1,31 +1,61 @@
-import React, { useState } from 'react';
+import React, { useRef, useContext } from 'react';
 import './ModalUser.css';
+import { context } from '../../Header/Header';
 
-const ModalUser = ({ id,image ,handleChangeImage}) => {
+const ModalUser = ({ id }) => {
+  const { image, setImage } = useContext(context);
+  const fileInputRef = useRef();
+
+  const handleSelectImage = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImage(URL.createObjectURL(file));
+    }
+  };
+
   return (
     <div className="modal fade" id={id} tabIndex="-1">
       <div className="modal-dialog">
         <div className="modal-content bg-dark">
 
           <div className="modal-header d-flex flex-column justify-content-center align-items-center">
-            <img src={image} className='imageUser rounded-circle mb-3 ' alt="" />
-            <button className="btn btn-close X bg-light position-absolute end-0 top-0 m-2" data-bs-dismiss="modal"></button>
+            <img 
+              src={image}
+              onClick={handleSelectImage}
+              className="imageUser rounded-circle mb-3" 
+              alt="user"
+              role="button"
+            />
+
+            <input 
+              accept="image/*"
+              ref={fileInputRef}
+              onChange={handleImageChange}
+              type="file"
+              style={{ display: "none" }}
+            />
+
+            <button 
+              className="btn btn-close X bg-light position-absolute end-0 top-0 m-2" 
+              data-bs-dismiss="modal">
+            </button>
           </div>
 
           <div className="modal-body">
-            <div className="input-group mb-3">
-              <span className="input-group-text">@</span>
-              <input type="text" className="form-control" />
-            </div>
+            <div className="form-floating mb-3">
+                    <input  type="text" className="form-control rounded-3" id="floatingInput" placeholder="userName" />
+                    <label for="floatingInput">Username</label>
+                    {false && <p className='text-danger'></p>}
+                </div>
 
-            <div>
-              <label>Email</label>
-              <input type="email" className="form-control" />
-            </div>
           </div>
 
           <div className="modal-footer">
-            <button className="btn btn-primary" data-bs-dismiss="modal">Submit</button>
+            <button className="btn btn-light text-dark" data-bs-dismiss="modal">Submit</button>
             <button className="btn btn-danger" data-bs-dismiss="modal">Close</button>
           </div>
 
