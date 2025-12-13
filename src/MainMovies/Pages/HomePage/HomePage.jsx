8577f2fee5f7ad68d4746e5a879fec8a51200b31
1ceyMan <nourachraf112@gmail.com>
2025-12-14 00:50:05 +0100

    Search result in a different section

--- src/MainMovies/Pages/HomePage/HomePage.jsx
+++ src/MainMovies/Pages/HomePage/HomePage.jsx
@@ -1,22 +1,22 @@
-import React, { useEffect, useState } from 'react'
-import Header from '../../Header/Header'
+import { useEffect, useState } from 'react'
 import { BiSearch } from 'react-icons/bi'
 import './HomePage.css'
 import Card from '../../Card/Card'
-import {FadeLoader } from 'react-spinners'
-import Footer from '../../FooterFolder/Footer'
+import { FadeLoader } from 'react-spinners'
 import { useDebounce } from 'react-use'
+
 const HomePage = () => {
-  const [ListMovies, setMoviesList] = useState([])
+  const [ListMovies, setMoviesList] = useState([]);
+  const [searchList, setSearchList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [page, setPage] = useState(1);
-  const [searchTerm,setSearchTerm] = useState("");
-  const [DebounseSearchTerm,setDebounceSearchTerm] = useState('');
+  const [query, setQuery] = useState("");
+  const [DebounseSearchTerm, setDebounceSearchTerm] = useState('');
 
-  const FetchMovie = async (searchByTage) => {
+  const FetchMovie = async (page = 1) => {
     try {
       setIsLoading(true);
-      const response = isNaN(searchByTage) ? await fetch("http://localhost:3000/api/search?q="+searchByTage):await fetch('http://localhost:3000/api/movies?p=' + searchByTage);
+      const response = await fetch('http://localhost:3000/api/movies?p=' + page);
       if (response.ok) {
         const data = await response.json()
         console.log(data)
@@ -29,15 +29,33 @@ const HomePage = () => {
       setIsLoading(false);
     }
   }
-  const HandleChange = ()=>{
-    const id = document.getElementById('InputValue');
-    id.addEventListener('keypress',()=>{
-      if(searchTerm){
-          FetchMovie(searchTerm);
-      }else{
-        FetchMovie();
+
+  const FetchSearchQuery = async (query) => {
+    try {
+      setIsLoading(true);
+      const response = await fetch("http://localhost:3000/api/search?q=" + query);
+      if (response.ok) {
+        const data = await response.json()
+        console.log(data)
+        setSearchList(data)
       }
-  })
+
+    } catch (e) {
+      console.log(e)
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  const HandleChange = () => {
+    const id = document.getElementById('InputValue');
+    id.addEventListener('keypress', (e) => {
+      if (e.key === "Enter" && query.length > 0) {
+        FetchSearchQuery(query);
+      } else {
+        setSearchList([]);
+      }
+    })
   }
 
   /* useDebounce(
@@ -52,13 +70,17 @@ const HomePage = () => {
     FetchMovie();
   }, [])
 
-  useEffect(()=>{
+  useEffect(() => {
     HandleChange();
-  },[searchTerm])
+  }, [])
 
-  useEffect(()=>{
-      FetchMovie(page)
-  },[page])
+  useEffect(() => {
+    FetchMovie(page)
+  }, [page])
+
+  useEffect(() => {
+    FetchSearchQuery(query)
+  }, [query])
 
 
   return (
@@ -66,14 +88,15 @@ const HomePage = () => {
       <div className='menu mb-4'>
         <div className='search-bar col bg-dark'>
           <BiSearch size={30} className='search-icon' />
-          <input type="text" id='InputValue' onChange={e=>setSearchTerm(e.target.value)} placeholder='Search your movie' className='w-50' />
+          <input type="text" id='InputValue' onChange={e => setQuery(e.target.value)} placeholder='Search' className='w-50' />
         </div>
       </div>
       <div className='movies'>
         <div className="trending-movies">
           <p>Trending Movie</p>
+          {isLoading && <div className='loader'><FadeLoader color='white' size={100} /></div>}
           <div className='trending movies-show'>
-            {/*All Trending movies in API in Appwrite*/}
+            {searchList.results && searchList.results.map((movie) => <Card title={movie.title} poster_path={movie.poster_path} link={movie.link} />)}
           </div>
         </div>
         <div className="all-movies">
@@ -88,8 +111,8 @@ const HomePage = () => {
             <li className="page-item" onClick={() => setPage(prev => prev - 1 == 0 ? 1 : prev - 1)}><a className="page-link" href="#" >&lt;</a></li>
             <li className="page-item" onClick={() => setPage(1)}><a className="page-link" href="#">1</a></li>
             <li className="page-item" onClick={() => setPage(2)}><a className="page-link" href="#">2</a></li>
-            <li className="page-item"><a className="page-link" onClick={()=>setPage(3)} href="#">3</a></li>
-            <li className="page-item" onClick={()=>setPage(prev=>prev+1)}><a className="page-link" href="#">&gt;</a></li>
+            <li className="page-item"><a className="page-link" onClick={() => setPage(3)} href="#">3</a></li>
+            <li className="page-item" onClick={() => setPage(prev => prev + 1)}><a className="page-link" href="#">&gt;</a></li>
           </ul>
         </nav>
       </div>
